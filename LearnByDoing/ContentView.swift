b9c1b2448c4bdfc3a07fd33cb5301ce8ec5cc8c5
Tissiana Alves <altissiana@gmail.com>
2020-07-09 00:33:50 -0600

    Initial Commit

---
+++ LearnByDoing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnByDoing
+//
+//  Created by Tissiana Alves on 7/9/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
