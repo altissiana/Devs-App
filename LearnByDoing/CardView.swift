54069adee7f84ff58dbce57b4b79bf32a7c7c0d4
Tissiana Alves <altissiana@gmail.com>
2020-07-09 01:01:18 -0600

    added CardView class

---
+++ LearnByDoing/CardView.swift
@@ -0,0 +1,26 @@
+//
+//  CardView.swift
+//  LearnByDoing
+//
+//  Created by Tissiana Alves on 7/9/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    // MARK - CARD
+    
+    var body: some View {
+        ZStack {
+            Text("CARD")
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
